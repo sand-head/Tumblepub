480c35549541f02c6fdb4aeaff9483cc59968722
Justin Schweigert <justin@schweigert.dev>
2020-10-11 19:07:50 -0500

    Change webfinger content-type to match spec, add fields to link struct.

--- src/routes/well_known.rs
+++ src/routes/well_known.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use actix_web::{get, web, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
 
@@ -7,7 +9,31 @@ pub struct WebfingerReq {
 }
 
 #[derive(Serialize)]
-pub enum WebfingerLink {}
+pub struct WebfingerLink {
+  /// The relation of the link.
+  rel: String,
+  #[serde(rename = "type")]
+  /// The content type of the link's resource.
+  content_type: Option<String>,
+  /// The link itself.
+  href: Option<String>,
+  /// Zero or more key-value pairs, where the key is a language tag ("en-us", "fr", etc.)
+  /// or "und", and the value is a title in that given language.
+  /// https://tools.ietf.org/html/rfc7033#section-4.4.4.4
+  titles: Option<HashMap<String, String>>,
+  /// Zero or more key-value pairs that convey additional information about the relation.
+  /// https://tools.ietf.org/html/rfc7033#section-4.4.4.5
+  properties: Option<HashMap<String, String>>,
+  /// A concept carried over from OStatus, and is used by Mastodon to notify other instances
+  /// of a "follow" or "subscribe" authorization page, like so:
+  /// ```json
+  /// {
+  ///   "rel": "http://ostatus.org/schema/1.0/subscribe",
+  ///   "template": "https://mastodon.social/authorize_interaction?uri={uri}"
+  /// }
+  /// ```
+  template: Option<String>,
+}
 
 #[derive(Serialize)]
 pub struct WebfingerRes {
@@ -30,7 +56,7 @@ pub async fn webfinger(query: web::Query<WebfingerReq>) -> impl Responder {
       };
 
       HttpResponse::Ok()
-        .content_type("application/activity+json")
+        .content_type("application/jrd+json")
         .json(res)
     }
     None => HttpResponse::NotFound().finish(),
