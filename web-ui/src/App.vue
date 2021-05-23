633aafacb269f1caebb9a8fef260471f6e54415e
Justin Schweigert <justin@schweigert.dev>
2021-05-22 22:24:06 -0500

    Fix Prettier for Vue files

--- web-ui/src/App.vue
+++ web-ui/src/App.vue
@@ -10,31 +10,34 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
 
 <script lang="ts">
-import { useHead } from '@vueuse/head';
-import { defineComponent } from 'vue';
+  import { useHead } from '@vueuse/head';
+  import { defineComponent } from 'vue';
 
-export default defineComponent({
-  setup() {
-    useHead({
-      title: 'Vite App',
-      meta: [{
-        charset: 'utf-8',
-      }, {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1.0',
-      }]
-    });
-  }
-})
+  export default defineComponent({
+    setup() {
+      useHead({
+        title: 'Vite App',
+        meta: [
+          {
+            charset: 'utf-8',
+          },
+          {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1.0',
+          },
+        ],
+      });
+    },
+  });
 </script>
--- web-ui/src/components/Button.vue
+++ web-ui/src/components/Button.vue
@@ -1,11 +1,16 @@
 <template>
-  <button class="px-2 py-1 rounded transition duration-50" :class="classes[props.color]" @click="onClick">
+  <button
+    class="px-2 py-1 rounded transition duration-50"
+    :class="classes[props.color]"
+    @click="onClick"
+  >
     {{ props.label }}
   </button>
 </template>
 
 <script setup lang="ts">
-  import { defineEmit, defineProps } from "vue";
+  import { defineEmit, defineProps } from 'vue';
+
   const props = defineProps({
     label: {
       type: String,
@@ -17,13 +22,13 @@
       validator: (value) => {
         return typeof value === 'string' && value in ['primary', 'secondary'];
       },
-    }
+    },
   });
   const emit = defineEmit(['click']);
 
-  const classes: {[type: string]: string} = {
-    'primary': 'bg-purple-500 text-white hover:bg-purple-600',
-    'secondary': 'bg-gray-200 text-black hover:bg-gray-300'
+  const classes: { [type: string]: string } = {
+    primary: 'bg-purple-500 text-white hover:bg-purple-600',
+    secondary: 'bg-gray-200 text-black hover:bg-gray-300',
   };
   function onClick() {
     emit('click');
