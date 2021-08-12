60464fa5c77c61a87d00cb65487b2b992ac624b1
Justin Schweigert <justin@schweigert.dev>
2021-08-11 19:14:41 -0500

    Move theme folder, fix build.rs

--- tumblepub/build.rs
+++ tumblepub/build.rs
@@ -1,11 +1,31 @@
-fn main() -> Result<(), std::io::Error> {
-  npm_rs::NpmEnv::default()
-    .with_env("NODE_ENV", "production")
-    .set_path("../theme")
-    .init()
-    .install(None)
-    .run("build")
-    .exec()?;
+use std::process::{Command, Stdio};
+
+fn main() -> anyhow::Result<()> {
+  println!(r#"cargo:rerun-if-changed=build.rs"#);
+  println!(r#"cargo:rerun-if-changed=theme"#);
+
+  let dir = dunce::canonicalize("./theme")?;
+  let command = Command::new("npm")
+    .current_dir(dir.clone())
+    .stdout(Stdio::piped())
+    .arg("install")
+    .spawn()?
+    .wait_with_output()?;
+
+  if command.status.success() {
+    let command = Command::new("npm")
+      .current_dir(dir)
+      .stdout(Stdio::piped())
+      .args(&["run", "build"])
+      .spawn()?
+      .wait_with_output()?;
+
+    if !command.status.success() {
+      panic!("{}", String::from_utf8(command.stdout)?);
+    }
+  } else {
+    panic!("{}", String::from_utf8(command.stdout)?);
+  }
 
   Ok(())
 }
