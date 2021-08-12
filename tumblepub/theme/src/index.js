bcc81dba2cfa03274ef2ba064aa87b5e25daea68
Justin Schweigert <justin@schweigert.dev>
2021-08-12 17:35:15 -0500

    Inject CSS into the default theme

--- tumblepub/theme/src/index.js
+++ tumblepub/theme/src/index.js
@@ -1,1 +1,4 @@
 // todo: make this typescript, for eventual infinite scrolling I guess?
+import "./styles.css";
+
+console.log("howdy world!");
--- tumblepub/theme/webpack.config.js
+++ tumblepub/theme/webpack.config.js
@@ -1,10 +1,15 @@
 const path = require("path");
+
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const InlineChunkHtmlPlugin = require("react-dev-utils/InlineChunkHtmlPlugin");
+const HtmlInlineCssWebpackPlugin = require("html-inline-css-webpack-plugin").default;
+
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-  mode: "none",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
@@ -12,11 +17,28 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
     new HtmlWebpackPlugin({
       template: "./src/index.hbs",
       filename: "index.hbs",
       inject: true,
+      minify: false,
     }),
     new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/bundle/]),
+    new HtmlInlineCssWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  },
 };
