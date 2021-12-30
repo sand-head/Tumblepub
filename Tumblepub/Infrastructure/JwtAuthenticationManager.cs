28b27a5ebd9d0cb8738fd68bf614d4c942de5b25
Justin Schweigert <justin@schweigert.dev>
2021-12-29 20:24:27 -0600

    Build theme with web app, start adding JWT authentication

---
+++ Tumblepub/Infrastructure/JwtAuthenticationManager.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Tumblepub.Infrastructure;
+
+public record JwtTokenConfig(string Issuer, string Secret, int AccessTokenExpiration, int RefreshTokenExpiration);
+public record JwtAuthResult(string AccessToken, RefreshToken RefreshToken);
+public record RefreshToken(string Username, string Token, DateTime ExpireAt);
+
+public class JwtAuthenticationManager
+{
+    public IImmutableDictionary<string, RefreshToken> UsersRefreshTokensReadOnlyDictionary => _usersRefreshTokens.ToImmutableDictionary();
+    // todo: store in database
+    private readonly ConcurrentDictionary<string, RefreshToken> _usersRefreshTokens;
+    private readonly JwtTokenConfig _jwtTokenConfig;
+    private readonly byte[] _secret;
+
+    public JwtAuthenticationManager(JwtTokenConfig jwtTokenConfig)
+    {
+        _jwtTokenConfig = jwtTokenConfig;
+        _usersRefreshTokens = new ConcurrentDictionary<string, RefreshToken>();
+        _secret = Encoding.ASCII.GetBytes(jwtTokenConfig.Secret);
+    }
+
+    public JwtAuthResult GenerateTokens(string username, Claim[] claims, DateTime now)
+    {
+        var jwtToken = new JwtSecurityToken(
+            issuer: _jwtTokenConfig.Issuer,
+            claims: claims,
+            expires: now.AddMinutes(_jwtTokenConfig.AccessTokenExpiration),
+            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(_secret), SecurityAlgorithms.HmacSha256Signature));
+        var accessToken = new JwtSecurityTokenHandler().WriteToken(jwtToken);
+
+        var refreshToken = new RefreshToken(username, GenerateRefreshTokenString(), now.AddMinutes(_jwtTokenConfig.RefreshTokenExpiration));
+        _usersRefreshTokens.AddOrUpdate(refreshToken.Token, refreshToken, (s, t) => refreshToken);
+
+        return new JwtAuthResult(accessToken, refreshToken);
+    }
+
+    private static string GenerateRefreshTokenString()
+    {
+        var randomNumber = new byte[32];
+        using var randomNumberGenerator = RandomNumberGenerator.Create();
+        randomNumberGenerator.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
+    }
+}
--- Tumblepub/Program.cs
+++ Tumblepub/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using Tumblepub.EventSourcing.Extensions;
 using Tumblepub.GraphQL;
+using Tumblepub.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 // configure event sourcing
-builder.AddEventSourcing(builder.Configuration.GetConnectionString("Database"));
+builder.AddEventSourcing(config.GetConnectionString("Database"));
 
 // add GraphQL support using HotChocolate
 builder.Services
@@ -12,9 +17,38 @@ builder.Services
     .AddQueryType<Query>()
     .AddMutationType<Mutation>();
 
+builder.Services
+    .AddSingleton<JwtTokenConfig>(new JwtTokenConfig(config["LocalDomain"], config["JwtSecret"], 60 * 2, 60 * 24))
+    .AddSingleton<JwtAuthenticationManager>()
+    .AddAuthorization()
+    .AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = !builder.Environment.IsDevelopment();
+        x.SaveToken = true;
+        x.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidIssuer = config["LocalDomain"],
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(config["JwtSecret"])),
+            //ValidAudience = jwtTokenConfig.Audience,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.FromMinutes(1)
+        };
+    });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/@{name}", (string name) => $"Hello {name}!");
 
 // maps "/graphql" to the GraphQL API endpoint
 app.MapGraphQL();
