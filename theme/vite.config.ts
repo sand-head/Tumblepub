621996bae81838cf9390d1e70660fc2b9188f500
Justin Schweigert <justin@schweigert.dev>
2022-04-24 21:28:05 -0500

    Update dummy info

--- theme/vite.config.ts
+++ theme/vite.config.ts
@@ -6,6 +6,10 @@ import helpers from './helpers';
 
 const config: UserConfig = {
   plugins: [
+    viteSingleFile(),
+    createHtmlPlugin({
+      minify: true,
+    }),
     {
       ...handlebars({
         helpers,
@@ -16,7 +20,7 @@ const config: UserConfig = {
           Title: "justin",
           Description:
             "howdy! my name's Justin and this is a lil demo description for testing this theme<br><br>very cool!",
-          Avatar: "https://avatars.githubusercontent.com/u/25518361?v=4",
+          Avatar: "https://pbs.twimg.com/profile_images/1444764955505532929/1l5wrYIs_400x400.jpg",
           PreviousPage: null,
           NextPage: null,
           Posts: [
@@ -34,10 +38,6 @@ const config: UserConfig = {
       }),
       apply: 'serve',
     },
-    viteSingleFile(),
-    createHtmlPlugin({
-      minify: true,
-    }),
   ]
 };
 
